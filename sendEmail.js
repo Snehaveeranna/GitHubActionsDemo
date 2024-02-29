4df63993b77b8cc4bcc3b05d29ccbac2275b4581
Snehaveeranna <108810588+Snehaveeranna@users.noreply.github.com>
2024-02-29 12:51:30 +0530

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -1,7 +1,18 @@
 const fs = require('fs');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const htmlReport = fs.readFileSync('${{ github.workspace }}/apidog-reports/', 'utf-8');
+//const htmlReport = fs.readFileSync('${{ github.workspace }}/apidog-reports/', 'utf-8');
+const now = new Date();
+const timestamp = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}-${now.getHours().toString().padStart(2, '0')}-${now.getMinutes().toString().padStart(2, '0')}-${now.getSeconds().toString().padStart(2, '0')}-${now.getMilliseconds().toString().padStart(3, '0')}`;
+
+// Construct the dynamic file name
+const fileName = `apidog-report-${timestamp}-0.html`; // Assuming sequence is always 0, change it accordingly if needed
+
+// Construct the path to the HTML file using github.workspace
+const reportPath = path.join(process.env.GITHUB_WORKSPACE, 'apidog-reports', fileName);
+
+// Read the HTML file
+const htmlReport = fs.readFileSync(reportPath, 'utf-8');
 const msg = {
   to: 'snehav@anthology.com', // Change this to the recipient's email address
   from: 'ivpnotifications@products.anthology.com', // Change this to your verified sender email in SendGrid
