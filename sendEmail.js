2277b495f68ad00772abdf6b8ec12da71e7bdd16
Snehaveeranna <108810588+Snehaveeranna@users.noreply.github.com>
2024-02-29 15:26:30 +0530

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -1,14 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 const sgMail = require('@sendgrid/mail');
+
+// Set SendGrid API key
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// Construct the path to the directory where artifacts are stored
 const artifactsDirectory = path.join(process.env.GITHUB_WORKSPACE, 'apidog-reports');
+
+// Read the contents of the directory
 const files = fs.readdirSync(artifactsDirectory);
+
+// Find the file with the name starting with "apidog-report-"
 const reportFile = files.find(file => file.startsWith('apidog-report-'));
+
 if (reportFile) {
     // Construct the path to the HTML file
     const reportPath = path.join(artifactsDirectory, reportFile);
+
+    // Read the HTML file
     const htmlReport = fs.readFileSync(reportPath, 'utf-8');
+
+    // Construct the email message
     const msg = {
       to: 'snehav@anthology.com', // Change this to the recipient's email address
       from: 'ivpnotifications@products.anthology.com', // Change this to your verified sender email in SendGrid
@@ -16,21 +29,23 @@ if (reportFile) {
       text: 'APIdog test report is attached.',
       attachments: [
         {
-        content: htmlReport,
-        filename: 'apidog_report.html',
-        type: 'text/html',
-        disposition: 'attachment',
+          content: htmlReport,
+          filename: 'apidog_report.html',
+          type: 'text/html',
+          disposition: 'attachment',
         },
       ],
     };
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent successfully');
-  })
-  .catch((error) => {
-    console.error('Error occurred while sending email:', error.response?.body || error.message);
-  });
+
+    // Send email
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent successfully');
+      })
+      .catch((error) => {
+        console.error('Error occurred while sending email:', error.response?.body || error.message);
+      });
 } else {
     console.error('No report file found.');
 }
