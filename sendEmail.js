4bb103dae7e0af2aeef16dc24e0eea06038966b6
Snehaveeranna <108810588+Snehaveeranna@users.noreply.github.com>
2024-02-29 13:27:07 +0530

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -1,47 +1,26 @@
 const fs = require('fs');
-const path = require('path');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-// Construct the path to the directory where artifacts are stored
-const artifactsDirectory = path.join(process.env.GITHUB_WORKSPACE, 'apidog-reports');
-
-// Read the contents of the directory
-const files = fs.readdirSync(artifactsDirectory);
-
-// Find the file with the name starting with "apidog-report-"
-const reportFile = files.find(file => file.startsWith('apidog-report-'));
-
-if (reportFile) {
-    // Construct the path to the HTML file
-    const reportPath = path.join(artifactsDirectory, reportFile);
-
-    // Read the HTML file
-    const htmlReport = fs.readFileSync(reportPath, 'utf-8');
-
-   const msg = {
-      to: 'snehav@anthology.com', // Change this to the recipient's email address
-      from: 'ivpnotifications@products.anthology.com', // Change this to your verified sender email in SendGrid
-      subject: 'APIdog Test Report',
-      text: 'APIdog test report is attached.',
-      attachments: [
-        {
-          content: htmlReport,
-          filename: 'apidog_report.html',
-          type: 'text/html',
-          disposition: 'attachment',
-        },
-      ],
-    };
-
-    // Send email
-    sgMail
-      .send(msg)
-      .then(() => {
-        console.log('Email sent successfully');
-      })
-      .catch((error) => {
-        console.error('Error occurred while sending email:', error);
-      });
-} else {
-    console.error('No report file found.');
-}
+const htmlReport = fs.readFileSync('testArtifacts/apidog_report.html', 'utf-8');
+const msg = {
+  to: 'snehav@anthology.com', // Change this to the recipient's email address
+  from: 'ivpnotifications@products.anthology.com', // Change this to your verified sender email in SendGrid
+  subject: 'APIdog Test Report',
+  text: 'APIdog test report is attached.',
+  attachments: [
+    {
+      content: htmlReport,
+      filename: 'apidog_report.html',
+      type: 'text/html',
+      disposition: 'attachment',
+    },
+  ],
+};
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent successfully');
+  })
+  .catch((error) => {
+    console.error('Error occurred while sending email:', error);
+  });
