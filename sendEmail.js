15eb5424b8d41c9e17c6e5bd273adf1ca4f6632e
Snehaveeranna <108810588+Snehaveeranna@users.noreply.github.com>
2024-02-29 15:16:29 +0530

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -1,21 +1,28 @@
 const fs = require('fs');
+const path = require('path');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const htmlReport = fs.readFileSync('testArtifacts/apidog_report.html', 'utf-8');
-const msg = {
-  to: 'snehav@anthology.com', // Change this to the recipient's email address
-  from: 'ivpnotifications@products.anthology.com', // Change this to your verified sender email in SendGrid
-  subject: 'APIdog Test Report',
-  text: 'APIdog test report is attached.',
-  attachments: [
-    {
-      content: htmlReport,
-      filename: 'apidog_report.html',
-      type: 'text/html',
-      disposition: 'attachment',
-    },
-  ],
-};
+const artifactsDirectory = path.join(process.env.GITHUB_WORKSPACE, 'apidog-reports');
+const files = fs.readdirSync(artifactsDirectory);
+const reportFile = files.find(file => file.startsWith('apidog-report-'));
+if (reportFile) {
+    // Construct the path to the HTML file
+    const reportPath = path.join(artifactsDirectory, reportFile);
+    const htmlReport = fs.readFileSync('testArtifacts/apidog_report.html', 'utf-8');
+    const msg = {
+      to: 'snehav@anthology.com', // Change this to the recipient's email address
+      from: 'ivpnotifications@products.anthology.com', // Change this to your verified sender email in SendGrid
+      subject: 'APIdog Test Report',
+      text: 'APIdog test report is attached.',
+      attachments: [
+        {
+        content: htmlReport,
+        filename: 'apidog_report.html',
+        type: 'text/html',
+        disposition: 'attachment',
+        },
+      ],
+    };
 sgMail
   .send(msg)
   .then(() => {
@@ -24,3 +31,6 @@ sgMail
   .catch((error) => {
     console.error('Error occurred while sending email:', error);
   });
+} else {
+    console.error('No report file found.');
+}
